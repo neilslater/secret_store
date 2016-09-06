1c31eb81b7996440a86523801f71dfe9ea334146
Neil Slater <neil@aframe.com>
2016-09-06 18:24:27 +0100

    Move PBKDF out of Secret

--- spec/secret_spec.rb
+++ spec/secret_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 
 describe SecretStore::Secret do
   let(:example_label) { "example" }
-  let(:example_iv) { "UbJdZhd4MQQTvFf9OB3W6A==" }
-  let(:example_pbkdf2_salt) { "mHv4-vrSLVpXMDmra6k-fg==" }
-  let(:example_crypted_text) { "ZsjUIqTfvEaJok25n-kuwMi5lwOgYgZ3yyBai2lJEzA=" }
+  let(:example_iv) { "a9ETWqaR0_zKPwhv0dioOw==" }
+  let(:example_crypted_text) { "DTAP0J-fyloaQAQV9XMfiMxO6TWpYSsHTt-5LjncH3I=" }
+  let(:example_key) { Base64.urlsafe_decode64("zfOc-2CKnVA0l3vpIsZUr6lhgnGdBAuxcPIK_H9lsY4=") }
 
-  let(:example_password) { 'hidden' }
   let(:example_plaintext) { 'This is a secret!' }
 
   describe "class methods" do
     describe "#new" do
       it "creates valid, decryptable object" do
-        secret = SecretStore::Secret.new( example_label, example_iv, example_pbkdf2_salt, example_crypted_text )
-        expect( secret.decrypt_text( example_password ) ).to eql example_plaintext
+        secret = SecretStore::Secret.new( example_label, example_iv, example_crypted_text )
+        expect( secret.decrypt_text( example_key ) ).to eql example_plaintext
       end
     end
 
@@ -22,23 +21,22 @@ describe SecretStore::Secret do
         h = Hash[
           :label => example_label,
           :iv => example_iv,
-          :pbkdf2_salt => example_pbkdf2_salt,
           :crypted_text => example_crypted_text
         ]
         secret = SecretStore::Secret.from_h( h )
-        expect( secret.decrypt_text( example_password ) ).to eql example_plaintext
+        expect( secret.decrypt_text( example_key) ).to eql example_plaintext
       end
     end
 
     describe "#create_from_plaintext" do
       it "creates valid, decryptable object" do
-        secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_password )
-        expect( secret.decrypt_text( example_password ) ).to eql example_plaintext
+        secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_key )
+        expect( secret.decrypt_text( example_key) ).to eql example_plaintext
       end
 
       it "uses a different initial value each time" do
         ivs = [*0..10].map do
-          secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_password )
+          secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_key )
           secret.iv
         end
         expect( ivs.uniq ).to eql ivs
@@ -46,32 +44,24 @@ describe SecretStore::Secret do
 
       it "has a different crypted text each time" do
         crypted_texts = [*0..10].map do
-          secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_password )
+          secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_key )
           secret.crypted_text
         end
         expect( crypted_texts.uniq ).to eql crypted_texts
-      end
-
-      it "has a different pbkdf2_salt each time" do
-        salts = [*0..10].map do
-          secret = SecretStore::Secret.create_from_plaintext( example_label, example_plaintext, example_password )
-          secret.pbkdf2_salt
-        end
-        expect( salts.uniq ).to eql salts
       end
     end
   end
 
   describe "instance methods" do
-    subject { SecretStore::Secret.new( example_label, example_iv, example_pbkdf2_salt, example_crypted_text ) }
+    subject { SecretStore::Secret.new( example_label, example_iv, example_crypted_text ) }
 
     describe "#decrypt_text" do
       it "works when the password is correct" do
-        expect( subject.decrypt_text( example_password ) ).to eql example_plaintext
+        expect( subject.decrypt_text( example_key) ).to eql example_plaintext
       end
 
-      it "raises error when the password is incorrect" do
-        bad_passwords = ['', 'wrong', '21357451782478' ]
+      it "raises error when the key is incorrect" do
+        bad_passwords = ['', 'wrong', SecureRandom.random_bytes(32) ]
         bad_passwords.each do |bad_password|
           expect {
             subject.decrypt_text( bad_password )
@@ -79,35 +69,33 @@ describe SecretStore::Secret do
         end
       end
 
-      it "does not change iv, pbkdf2_salt, or crypted text" do
-        expect( subject.decrypt_text( example_password ) ).to eql example_plaintext
+      it "does not change iv, or crypted text" do
+        expect( subject.decrypt_text( example_key) ).to eql example_plaintext
 
         expect( subject.iv ).to eql example_iv
-        expect( subject.pbkdf2_salt ).to eql example_pbkdf2_salt
         expect( subject.crypted_text ).to eql example_crypted_text
       end
     end
 
     describe "#replace_text" do
-      it "works using same password" do
+      it "works using same key" do
         new_message = 'Another different secret!'
-        subject.replace_text( new_message, example_password )
-        expect( subject.decrypt_text( example_password ) ).to eql new_message
+        subject.replace_text( new_message, example_key)
+        expect( subject.decrypt_text( example_key) ).to eql new_message
       end
 
-      it "works using a new password" do
+      it "works using a new key" do
         new_message = 'Another different secret!'
-        new_password = 'thing'
-        subject.replace_text( new_message, new_password )
-        expect( subject.decrypt_text( new_password ) ).to eql new_message
+        new_key = SecureRandom.random_bytes(32)
+        subject.replace_text( new_message, new_key )
+        expect( subject.decrypt_text( new_key ) ).to eql new_message
       end
 
-      it "changes iv, pbkdf2_salt, and crypted text even if password and message are the same" do
-        subject.replace_text( example_plaintext, example_password )
-        expect( subject.decrypt_text( example_password ) ).to eql example_plaintext
+      it "changes iv, and crypted text even if password and message are the same" do
+        subject.replace_text( example_plaintext, example_key )
+        expect( subject.decrypt_text( example_key) ).to eql example_plaintext
 
         expect( subject.iv ).to_not eql example_iv
-        expect( subject.pbkdf2_salt ).to_not eql example_pbkdf2_salt
         expect( subject.crypted_text ).to_not eql example_crypted_text
       end
     end
@@ -116,10 +104,9 @@ describe SecretStore::Secret do
       it "serialises the object" do
         h = subject.to_h
         secret = SecretStore::Secret.from_h( h )
-        expect( secret.decrypt_text( example_password ) ).to eql example_plaintext
+        expect( secret.decrypt_text( example_key ) ).to eql example_plaintext
         expect( subject.label ).to eql example_label
         expect( subject.iv ).to eql example_iv
-        expect( subject.pbkdf2_salt ).to eql example_pbkdf2_salt
         expect( subject.crypted_text ).to eql example_crypted_text
       end
     end
